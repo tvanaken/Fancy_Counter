970c0b1f64f78df2906ef1f7222c64979a84d194
tvanaken <89529903+tvanaken@users.noreply.github.com>
2024-08-25 16:09:00 -0400

    Restart

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,13 @@ import Button from "./Button";
 import { useState } from "react";
 
 function App() {
-	const [number, setNumber] = useState(0);
-	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				justifyContent: "center",
-			}}
-		>
-			<Count number={number} />
-			<Button setNumber={setNumber} />
-		</div>
-	);
+    const [number, setNumber] = useState(0);
+    return (
+        <div className="app">
+            <Count number={number} />
+            <Button setNumber={setNumber} />
+        </div>
+    );
 }
 
 export default App;
--- src/Count.jsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export default function Count({ number }) {
-	useEffect(() => {
-		document.title = `Count: ${number}`;
-	}, [number]);
-	return <span className="count">{number}</span>;
-}
